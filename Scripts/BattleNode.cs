b121158a26861d747c47949972fad63254998230
max-r-johnson <max.r.johnson2021@gmail.com>
2025-04-11 21:58:12 -0500

    FIx Project

--- Scripts/BattleNode.cs
+++ Scripts/BattleNode.cs
@@ -1,26 +1,27 @@
 using Godot;
+using System;
 
 public partial class BattleNode : Node
 {
-    public override void _Ready()
-    {
-        Unit murderer = new Murderer();
-        Subjects subjects = new Subjects([murderer]);
-        GD.Print(subjects.ToString());
-        instantiateUnits(subjects);
-    }
+	public override void _Ready()
+	{
+		Unit murderer = new Murderer();
+		Subjects subjects = new Subjects([murderer]);
+		GD.Print(subjects.ToString());
+		instantiateUnits(subjects);
+	}
 
-    public void instantiateUnits(Subjects subjects)
-    {
-        var unitScene = GD.Load<PackedScene>("res://Scenes/Unit.tscn");
-        foreach (Unit unit in subjects.units)
-        {
-            foreach (int i in GD.Range(subjects.unitQuantities[unit]))
-            {
-                var unitInstance = unitScene.Instantiate();
-                unitInstance.Name = unit.GetType().Name;
-                AddChild(unitInstance);
-            }
-        }
-    }
+	public void instantiateUnits(Subjects subjects)
+	{
+		foreach (Unit unit in subjects.units)
+		{
+			foreach (int i in GD.Range(subjects.unitQuantities[unit]))
+			{
+				var unitScene = GD.Load<PackedScene>("res://Scenes/Unit.tscn");
+				var unitInstance = unitScene.Instantiate();
+				unitInstance.Name = unit.GetType().Name;
+				AddChild(unitInstance);
+			}
+		}
+	}
 }
