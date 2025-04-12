2d33c752fcc4b49bd4302248e876c66437d84ace
NJ <njankow003@gmail.com>
2025-04-12 01:15:15 -0500

    Setting gameplan

--- Scripts/Shop.cs
+++ Scripts/Shop.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 public partial class Shop
 {
-    public List<Upgrade> availableUpgrades { get; set; }
+    public Hashset<Upgrade> availableUpgrades { get; set; }
     public List<Upgrade> currentUpgrades { get; set; }
     public int rollAmount { get; set; }
     public Shop()
@@ -17,5 +17,10 @@ public partial class Shop
     {
         GameManager.Game.subjects.decLives(rollAmount);
     }
+
+    //Roll shop after each round and at start of game, or after reroll
+    //For each roll, take upgrades from availableUpgrades, add them to currentUpgrades, and display them
+    //Randomly display 6 upgrades, possibly reduce to 4, duplicates allowed but no more than 2
+    //Upgrade is an object
    
 }
