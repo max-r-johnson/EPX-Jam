e64984fd2a923e45b4b10be5a285757fb90d6156
NJ <njankow003@gmail.com>
2025-04-12 23:00:39 -0500

    changes

--- Scripts/Upgrade.cs
+++ Scripts/Upgrade.cs
@@ -6,16 +6,10 @@ using System.Threading.Tasks;
 public partial class Upgrade
 {
     public int cost { get; set; }
-<<<<<<< HEAD
-    public float costMultiplier { get; set; } = 1;
-    public Game game {get {return GameManager.Game;}}
-    public Upgrade ()
-=======
     public string text { get; set; }
     public Game game { get { return GameManager.Game; }}
     public Shop shop { get { return game.shop; }}
     public Upgrade()
->>>>>>> Max2
     {
         cost = 3;
     }
--- Scripts/UpgradeList.cs
+++ Scripts/UpgradeList.cs
@@ -6,17 +6,15 @@ public partial class MurdererUpgrade : Upgrade
 {
     public MurdererUpgrade() : base()
     {
-<<<<<<< HEAD
-        cost = 1;
-        game.subjects.unitTypes[0].stats[]
-=======
         text = "Upgrade the base stats of your murderers.";
     }
 
     public override void upgradeMethod()
     {
-
->>>>>>> Max2
+        game.subjects.unitTypes[0].stats["attack"] += 1;
+        game.subjects.unitTypes[0].stats["health"] += 3;
+        game.subjects.unitTypes[0].stats["attack speed"] *= 1.1f;
+        game.subjects.unitTypes[0].stats["movement speed"] *= 1.1f;
     }
 }
 
@@ -24,15 +22,14 @@ public partial class LooterUpgrade : Upgrade
 {
     public LooterUpgrade() : base()
     {
-<<<<<<< HEAD
-        cost = 1;
-=======
         text = "Upgrade the base stats of your looters.";
     }
     public override void upgradeMethod()
     {
-        
->>>>>>> Max2
+        game.subjects.unitTypes[1].stats["attack"] += 0.5f;
+        game.subjects.unitTypes[1].stats["health"] += 2;
+        game.subjects.unitTypes[1].stats["attack speed"] *= 1.1f;
+        game.subjects.unitTypes[1].stats["movement speed"] *= 1.1f;
     }
 }
 
@@ -40,15 +37,11 @@ public partial class Pride : Upgrade
 {
     public Pride() : base()
     {
-<<<<<<< HEAD
-        cost = 1;
-=======
         text = "";
     }
     public override void upgradeMethod()
     {
         
->>>>>>> Max2
     }
 }
 
@@ -56,19 +49,15 @@ public partial class Wrath : Upgrade
 {
     public Wrath() : base()
     {
-<<<<<<< HEAD
-        cost = 1;
-=======
         text = "Upgrade the attack of your units by 10%.";
     }
     public override void upgradeMethod()
     {
         foreach(Unit unitType in game.subjects.unitTypes)
         {
-            unitType.stats["attack"] *= 1.25f;
+            unitType.stats["attack"] *= 1.1f;
             GD.Print(unitType.stats["attack"]);
         }
->>>>>>> Max2
     }
 }
 
@@ -76,15 +65,17 @@ public partial class Sloth : Upgrade
 {
     public Sloth() : base()
     {
-<<<<<<< HEAD
-        cost = 1;
-=======
         text = "Slow enemy movement and attack speed by 10%.";
     }
     public override void upgradeMethod()
     {
-        
->>>>>>> Max2
+        foreach(Unit unitType in game.enemySubjects.unitTypes)
+        {
+            unitType.stats["attack speed"] *= 0.9f;
+            GD.Print(unitType.stats["attack speed"]);
+            unitType.stats["movement speed"] *= 0.9f;
+            GD.Print(unitType.stats["movement speed"]);
+        }
     }
 }
 
@@ -92,16 +83,12 @@ public partial class Lust : Upgrade
 {
     public Lust() : base()
     {
-<<<<<<< HEAD
-        cost = 1;
-=======
         text = "Gain more lives at the beginning of each turn.";
     }
     public override void upgradeMethod()
     {
         shop.roundLives += 1;
 		GD.Print("new round lives: " + shop.roundLives);
->>>>>>> Max2
     }
 }
 
@@ -109,15 +96,11 @@ public partial class Greed : Upgrade
 {
     public Greed() : base()
     {
-<<<<<<< HEAD
-        cost = 1;
-=======
         text = "";
     }
     public override void upgradeMethod()
     {
         
->>>>>>> Max2
     }
 }
 
@@ -125,15 +108,30 @@ public partial class Envy : Upgrade
 {
     public Envy() : base()
     {
-<<<<<<< HEAD
-        cost = 1;
-=======
         text = "If you have a unit the last enemy had, level it up.";
     }
     public override void upgradeMethod()
     {
-        
->>>>>>> Max2
+        foreach(Unit unitType in game.enemySubjects.unitTypes)
+        {
+            if (game.subjects.unitTypes.Contains(unitType))
+            {
+                if (unitType is Murderer)
+                {
+                    game.subjects.unitTypes[1].stats["attack"] += 0.5f;
+                    game.subjects.unitTypes[1].stats["health"] += 2;
+                    game.subjects.unitTypes[1].stats["attack speed"] *= 1.1f;
+                    game.subjects.unitTypes[1].stats["movement speed"] *= 1.1f;
+                }
+                else if (unitType is Looter)
+                {
+                    game.subjects.unitTypes[1].stats["attack"] += 0.5f;
+                    game.subjects.unitTypes[1].stats["health"] += 2;
+                    game.subjects.unitTypes[1].stats["attack speed"] *= 1.1f;
+                    game.subjects.unitTypes[1].stats["movement speed"] *= 1.1f;
+                }
+            }
+        }
     }
 }
 
@@ -141,14 +139,14 @@ public partial class Gluttony : Upgrade
 {
     public Gluttony() : base()
     {
-<<<<<<< HEAD
-        cost = 1;
-=======
-        text = "";
+        text = "Upgrade the health of your units by 10%.";
     }
     public override void upgradeMethod()
     {
-        
->>>>>>> Max2
+        foreach(Unit unitType in game.subjects.unitTypes)
+        {
+            unitType.stats["health"] *= 1.1f;
+            GD.Print(unitType.stats["health"]);
+        }
     }
 }
